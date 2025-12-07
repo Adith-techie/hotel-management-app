31f8be27b89335ba004b706ffc3ba72316c180db
Adith-techie <adithamith810@gmail.com>
2025-12-07 22:27:58 +0530

    n

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  // normal build (Vercel): base = '/'
-  // GitHub Pages build: base = '/hotel-management-app/'
+  // For Vercel (normal build): base = '/'
+  // For GitHub Pages (gh build): base = '/hotel-management-app/'
   base: mode === "gh-pages" ? "/hotel-management-app/" : "/",
 }));
